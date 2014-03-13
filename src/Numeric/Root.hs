d1f32e2c878777d710291e6159000c63369f9ebc
Hiromi Ishii <konn.jinro@gmail.com>
2014-03-14 00:12:20 +0900

    Implemented false-position method and secant method

--- src/Numeric/Root.hs
+++ src/Numeric/Root.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE BangPatterns #-}
 {-# OPTIONS_GHC -funbox-strict-fields #-}
-module Root where
+module Numeric.Root where
 import Data.Maybe
 
 data Interval a = Interval { lowerBound :: !a
@@ -70,3 +70,48 @@ bisect try prec f (Interval x1 x2)
                then Just rtb'
                else loop (n+1) dx' rtb'
     in loop (0 :: Int) dx0 rtb0
+
+falsePosition :: (Fractional a, Ord a, Eq a, Num a)
+              => Int            -- ^ max iteration
+              -> a              -- ^ precision
+              -> (a -> a)       -- ^ function to find roots
+              -> Interval a     -- ^ bracketing interval
+              -> Maybe a        -- ^ root in the given interval
+falsePosition its prec fun (Interval x1 x2) =
+  let (xl0, xh0)
+        | fun x1 < 0  = (x1, x2)
+        | otherwise = (x2, x1)
+      loop !n !xl !xh
+        | n >= its = Nothing
+        | otherwise =
+          let !fl = fun xl; !fh = fun xh
+              !dx = xl - xh
+              !rtf = xl + dx * fl / (fl - fh)
+              !f = fun rtf
+              (!del, xl', xh') =
+                if f < 0
+                then (xl-rtf, rtf, xh)
+                else (xh-rtf, xl, rtf)
+          in if abs del < prec || f == 0
+             then Just rtf
+             else loop (n+1) xl' xh'
+  in loop 0 xl0 xh0
+
+secant :: (Fractional a, Ord a, Eq a, Num a)
+          => Int -> a -> (a -> a) -> Interval a -> Maybe a
+secant its prec fun (Interval x1 x2) =
+  let (rts0, xl0, fl0, f0)
+        | abs (fun x1) < abs (fun x2) = (x1, x2, fun x2, fun x1)
+        | otherwise = (x2, x1, fun x1, fun x2)
+      loop !n !rts !xl !fl !f
+        | n >= its  = Nothing
+        | otherwise =
+          let dx = (xl - rts)*f/(f-fl)
+              xl' = rts
+              fl' = f
+              rts' = rts + dx
+              f' = fun rts'
+          in if abs dx < prec || f == 0
+             then Just rts'
+             else loop (n+1) rts' xl' fl' f'
+  in loop 0 rts0 xl0 fl0 f0
